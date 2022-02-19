39318a6f2a93e30723be9980348ddf95827d28c8
hiroto302 <i.am.a.hiroto@gmail.com>
2022-02-20 03:57:36 +0900

    update PropertyExample.cs
    自動実装プロパティーのバッキングフィールドに属性をつけれるようになった ことについて

--- Assets/Scripts/C#/Format/PropertyExample.cs
+++ Assets/Scripts/C#/Format/PropertyExample.cs
@@ -36,16 +36,25 @@ namespace Format
             set { myBackingField = value; }
         }
 
+
         void Start()
         {
             Rect1 rect1  = new Rect1(1.0f, 1.0f);
+
             Player1 player1 = new Player1();
             Debug.Log(player1.Name);
             Debug.Log(player1.Level);
             Player2 player2 = new Player2();
             Debug.Log(player2.Name);
             Debug.Log(player2.Level);
+
+            Debug.Log(Example2.MyProperty1 + " : ex2.property1");
+            Example2.DisplayNum();
         }
+        [SerializeField] BackingFieldAttributeExample Example = new BackingFieldAttributeExample();
+        [SerializeField] BackingFieldAttributeExample1 Example1 = new BackingFieldAttributeExample1();
+        [SerializeField] BackingFieldAttributeExample2 Example2 = new BackingFieldAttributeExample2();
+
     }
 
     /* 自動実装プロパティーの強化
@@ -132,4 +141,74 @@ namespace Format
         public string Name { get; set; } = "Player2 Name";
         public int Level { get; set; } = 2;
     }
+
+    // 3. 自動実装プロパティーのバッキングフィールドに属性をつけれるようになった ことについて
+    [System.Serializable]
+    public class BackingFieldAttributeExample
+    {
+        // 下記の変数については、シリアライズ化されない
+        [System.NonSerialized] public int myProperty;
+        [SerializeField] int myProperty1;
+        public int MyProperty { get; private set; }
+    }
+    // C#7.3 より前では, 自動実装プロパティーの バッキングフィールドに NonSerialized などの属性を付与することがで出来なっかため下記のように記述するしかなかった
+    [System.Serializable]
+    public class BackingFieldAttributeExample1
+    {
+        [System.NonSerialized]
+        int field;
+        public int MyProperty
+        {
+            get { return field; }
+            set { field = value; }
+        }
+        [SerializeField]
+        int field1;
+        public int MyProperty2
+        {
+            get { return field1; }
+            set { field1 = value; }
+        }
+    }
+    // 上記の記述が下記のようにできるようになった
+    [System.Serializable]
+    public class BackingFieldAttributeExample2
+    {
+        [field: System.NonSerialized]
+        public int MyProperty { get; private set; }
+        [field: SerializeField]
+        public int MyProperty1 { get; private set; }
+        public void DisplayNum()
+        {
+            Debug.Log(MyProperty1);
+        }
+    }
+    /*
+
+    ここまで記述してきたが、[field: System.NonSerialized]は、そもそも Property に関しては Inspecter 上に表示されない。どこで有用的に使うことができるのか分からない。
+    変更しないようにすること明示的するためか？
+    しかし、[field: SerializeField] は プロパティーを表示することができるから有効できるから有効的だ！と思えるがこの使用方法は問題があり避けるべきである
+    BackingFieldAttributeExample2 の インスタンス を生成し、Inspector 上の値を確認すると、 My Property1 : 0 となっている。 My Property1 というような名称でシリアライズ化されている。
+    <MyProperty1>k__BackingField : 0 は、自動実装プロパティーの内部的に生成されるバッキングフィールドの名称である。コードからアクセスできず、
+    普通のフィールドとしては使えない名称のフィールドとなっている
+    これに、SerializeField を付与すると、普通の名称としては使えない名称でシリアライズされることに注意しなければならない。
+    今使っている限り、インスペクタ上の表示される名称は改良されている。<MyProperty1>k__BackingField のような名称で表示されていた。
+    データ(YAML形式)の方を確認すると次のような名前でシリアライズ化されている
+
+    Example2:
+    <MyProperty1>k__BackingField: 20
+
+    「シリアライズされる名前」・「シリアライズされたり、永続化される際のデータフォーマット」という観点からは、やはり考慮しないといけないかも
+    現状の [field: SerializeField] を使うのは、特にプロダクトでは避けたほうが賢明。
+    バッキングフィールドの内部表現が<***>k__BackingFieldであり続ける保証がないので、シリアライズしたデータと齟齬が生まれる可能性を排除できないから。
+
+    なので、
+
+    [SerializeField] private int level;
+    public int Level => level;
+
+    のような書き方をしていこう!
+    「SerializeFieldをつけた時のシリアライズされる名前はフィールド名で」っていう原則が崩さないようにする
+
+    */
 }
