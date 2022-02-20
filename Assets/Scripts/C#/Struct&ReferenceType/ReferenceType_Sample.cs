98ae76f780dd34808f35b8958a521ae0f940aeb3
hiroto302 <i.am.a.hiroto@gmail.com>
2022-02-20 22:43:10 +0900

    Readonly 修飾子について
    防御的コピー

--- Assets/Scripts/C#/Struct&ReferenceType/ReferenceType_Sample.cs
+++ Assets/Scripts/C#/Struct&ReferenceType/ReferenceType_Sample.cs
@@ -12,4 +12,27 @@ namespace ReferenceType
         }
     }
 
+    // readonly 修飾子について
+    public class ReadOnlyExample
+    {
+        public readonly string readonlyField;
+        readonly List<string> readonlyFieldList;
+
+        public ReadOnlyExample(string initialValue)
+        {
+            readonlyField = initialValue;
+            readonlyFieldList.Add(initialValue);
+        }
+
+        public void Update(string updateValue)
+        {
+            // readonly フィールドは上書きできない
+            // readonlyField = updateValue;
+
+            // 上記のように上書きは出来ない
+            // readonlyField[0] = updateValue;
+            // ただし、状態の更新は可能
+            readonlyFieldList.Add(updateValue);
+        }
+    }
 }
--- Assets/Scripts/C#/Struct&ReferenceType/Struct_Sample.cs
+++ Assets/Scripts/C#/Struct&ReferenceType/Struct_Sample.cs
@@ -8,6 +8,55 @@ namespace Structure
 {
     public class Struct_Sample : MonoBehaviour
     {
+        void Start()
+        {
+            ReadOnlyExample readOnlyExample = new ReadOnlyExample();
+            readOnlyExample.UpdateStructExample();
+        }
+    }
 
+    // readonly 修飾子について
+    // 構造体において、readonly フィールドでは 防御的コピーが生じることがある。
+    // 構造体の StructExample を作成
+    public struct StructExample
+    {
+        int value;
+        public StructExample(int initialValue)
+        {
+            value = initialValue;
+        }
+        public void Update(int updateValue)
+        {
+            value = updateValue;
+            Debug.Log(value + " : 呼び出す構造体メソッド内のフィールド");
+        }
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+    public class ReadOnlyExample
+    {
+        // 普通のフィールド
+        public StructExample normalField = new StructExample(1);
+        // readonly フィールド
+        public readonly StructExample readOnlyField = new StructExample(1);
+
+        public void UpdateStructExample()
+        {
+            // 普通のフィールド
+            // Debug.Log(normalField); // 1
+            // normalField.Update(5);  // 値が更新される
+            // Debug.Log(normalField); // 5
+
+            /* 防御的コピーについて
+            「 readonly 修飾子がついている構造体」の中身の書き換えを防ぐための仕組み
+            防御的コピーにより、readonly修飾子がついている構造体のフィールドのメソッド呼び出しでは、
+            そのフィールドのオブジェクトのコピーが発生する。そして、コピーしたオブジェクトがメソッドを呼び出す。
+            */
+            Debug.Log(readOnlyField); // 1
+            readOnlyField.Update(5);  // 防御的コピーが実行され、コピーされた構造体が５に更新される
+            Debug.Log(readOnlyField); // 1
+        }
     }
 }
