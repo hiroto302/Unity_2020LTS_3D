590cb0f93b239decc70d37835d71c7fb493dacea
hiroto302 <i.am.a.hiroto@gmail.com>
2022-02-20 22:07:10 +0900

    構造体 と 参照型について
    Create Struct_Sample.cs, ReferenceType_Sample.cs

---
+++ Assets/Scripts/C#/Struct&ReferenceType/ReferenceType_Sample.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ReferenceType
+{
+    public class ReferenceType_Sample : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/C#/Struct&ReferenceType/Struct_Sample.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 構造体は、別の変数へ代入する時や引数として渡す時、そして返り値として返す時はコピーが発生する
+// 参照型との違いを意識しながら実装していくこと
+namespace Structure
+{
+    public class Struct_Sample : MonoBehaviour
+    {
+
+    }
+}
