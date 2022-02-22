876c3e4456995f9b55cb06e85910c0b082e47855
hiroto302 <i.am.a.hiroto@gmail.com>
2022-02-22 13:37:18 +0900

    Update SubscribeSample.cs
    メッセージ購読について追加
    購読・ブロードキャスト
    Subscribe について

--- Assets/Scripts/UniRx/SubscribeSample.cs
+++ Assets/Scripts/UniRx/SubscribeSample.cs
@@ -8,6 +8,9 @@ UniRxにおいて、イベントメッセージを購読する場合、「IObser
 しかし、都度 Observerオブジェクトを定義し、インスタンス化するのはかったるい。(MySubjectUseSample.csでやったように)
 そのため、UniRx では 「IObservable<T> に登録する Observerオブジェクトをデリゲートから生成できる拡張メソッド」が用意されている
 この拡張メソッドを用いると、「ラムダ式で Subscribe() よび出す時に、イベントのハンドリング内容を記述する」ことが可能となる
+
+Subscribe 購読 : 観察者?が発したメッセージを購読（受け取る）する。(サイトによって、Observer と Subject との関係性の説明が違う)
+ブロードキャスト : 通信・ネットワークの分野ではネットワークに参加するすべての機器に同時に信号やデータを送信することを意味する
 */
 namespace UniRxSample
 {
@@ -18,6 +21,8 @@ namespace UniRxSample
             // Subject を生成
             var subject = new Subject<string>();
 
+            // subject に Observer オブジェクトを登録する。イベントが発行され時、 subjectに 登録されている observer に イベントメッセージのブロードキャストが行われる。
+            // そのブロードキャスト(メッセージ) を受信(購読している) observer が実行する、イベントのハンドリング内容を下記に記述する
             subject
                 .Subscribe(
                     onNext: x => Debug.Log(x),
