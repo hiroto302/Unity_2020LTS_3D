5572365aeb26728d0ea07a8281fd6ca3538a3733
hiroto302 <i.am.a.hiroto@gmail.com>
2022-02-25 01:01:18 +0900

    UniTask Sample 0
    WhenAll , Delay

--- Assets/Scripts/Asynchronous/UniTask/Introduction/UniTask_Sample.cs
+++ Assets/Scripts/Asynchronous/UniTask/Introduction/UniTask_Sample.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using System;
-using UnityEngine.Networking;
 using Cysharp.Threading.Tasks; // これがいる , 昔は UniRx.Async だった
 
 
 /*
 UniTaskとはUnity向けに最適化されたTask実装を提供するライブラリ。
 もとはUniRxに組み込まれていましたが途中で分離し、独立した1つのライブラリとして提供されるようになった。
-
-
 */
 
-namespace UniTask.Sample
+
+public class UniTask_Sample : MonoBehaviour
 {
-    public class UniTask_Sample : MonoBehaviour
-    {
-       
-    }
+        async UniTask Start()
+        {
+            var sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            // 約3000 ミリ秒で終わる
+            await Example();
+            await Example();
+            await Example();
+            Debug.Log($"{sw.ElapsedMilliseconds} : 3つのTask処理完了");
+
+            // 約 1000 ミリ秒で終わる
+            // WhenAll : 引数に与えた UniTask の処理全てが終了したら await を抜ける
+            await UniTask.WhenAll(Example(), Example(), Example());
+            Debug.Log($"{sw.ElapsedMilliseconds} : 同時に3つのTask処理完了");
+        }
+
+        async UniTask Example()
+        {
+            // 1000 ミリ秒で終わる UniTask
+            await UniTask.Delay(1000);
+        }
 }
