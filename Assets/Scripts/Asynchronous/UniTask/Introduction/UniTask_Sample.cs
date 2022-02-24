06e7a5b8ff6bf14a6f35f6ccda7747823f3e4180
hiroto302 <i.am.a.hiroto@gmail.com>
2022-02-25 00:43:58 +0900

    フォルダ整理

---
+++ Assets/Scripts/Asynchronous/UniTask/Introduction/UniTask_Sample.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Networking;
+using Cysharp.Threading.Tasks; // これがいる , 昔は UniRx.Async だった
+
+
+/*
+UniTaskとはUnity向けに最適化されたTask実装を提供するライブラリ。
+もとはUniRxに組み込まれていましたが途中で分離し、独立した1つのライブラリとして提供されるようになった。
+
+
+*/
+
+namespace UniTask.Sample
+{
+    public class UniTask_Sample : MonoBehaviour
+    {
+       
+    }
+}
