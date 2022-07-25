0b6334a195ee82124cd32fc44cdf4876622a0dc4
TaniguchiHiroto <55151347+hiroto302@users.noreply.github.com>
2022-07-26 00:18:18 +0900

    Update DisplayMsg.cs
    Comolete が最後に実行されるかチェック

--- Assets/Scripts/UniRx_TorisoupQiita_Inrotoduction/No1/DisplayMsg.cs
+++ Assets/Scripts/UniRx_TorisoupQiita_Inrotoduction/No1/DisplayMsg.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace ToriQita1
@@ -25,8 +26,17 @@ namespace ToriQita1
             msgSubject.OnNext("ハロー ワールド");
             
             // ShowMsg("Hello !!");
+            
+            // Complete の実験を 追加
+            this.UpdateAsObservable()
+                .Where(_ => Input.GetKeyDown(KeyCode.D))
+                .Subscribe(_ => ShowMsg("AAA"), () => Debug.Log("CompleteUpdateAsObservable"))
+                .AddTo(this);
         }
 
+        // OnNext : 通常イベントが発行されたときに通知されるメッセージ
+        // OnError : ストリームの処理中で例外が発生したことを通知する
+        // OnCompleted: ストリームが終了したことを通知する
         void ShowMsg(string msg)
         {
             Subject<string> subject = new Subject<string>();
@@ -53,6 +63,7 @@ namespace ToriQita1
                 () => Debug.Log($"Complete 4"));
             
             subject.OnNext(msg);
+            subject.OnCompleted();
         }
     }
 }
